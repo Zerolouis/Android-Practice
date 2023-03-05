9c1fc3585f30983792cf54d7ba7a4e7fdfcd82ee
Zerolouis <wxjjuju0322@gmail.com>
2023-03-05 19:52:51 +0800

    首次提交

---
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myfirstapplication;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	private int mCount = 0;
+	private TextView mShowCount;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Log.d("Create","初始化完成");
+		mShowCount = (TextView) findViewById(R.id.show_count);
+	}
+
+	public void showToast(View view) {
+		Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
+	public void countUp(View view) {
+		mCount++;
+		if(mShowCount!=null){
+			mShowCount.setText(Integer.toString(mCount));
+		}
+	}
+}
---
+++ ScrollingText/app/src/main/java/com/zerolouis/scrollingtext/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zerolouis.scrollingtext;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
